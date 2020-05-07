76d98fb58b1ab6fff5d4987a19aac0944471caf0
Boudewijnvdb <62020353+Boudewijnvdb@users.noreply.github.com>
2020-05-07 17:54:26 +0200

    Add files via upload

---
+++ Eindopdracht:KleurenToggle/script.js
@@ -0,0 +1,11 @@
+const knop = document.querySelector(".btn-toggle-nav");
+
+const navi = document.querySelector(".nav-sidebar");
+
+
+knop.addEventListener('click', function () {
+    navi.style.visibility = "visible"
+});
+
+
+
