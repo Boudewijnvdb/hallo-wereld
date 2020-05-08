9947f97c68081fb02805a901f69b0ab34ee6e510
Boudewijnvdb <62020353+Boudewijnvdb@users.noreply.github.com>
2020-05-08 08:48:35 +0200

    Update script.js

--- Eindopdracht:KleurenToggle/script.js
+++ Eindopdracht:KleurenToggle/script.js
@@ -1,3 +1,5 @@
+
+//menu toggle
 const knop = document.querySelector(".btn-toggle-nav");
 
 const navi = document.querySelector(".nav-sidebar");
@@ -6,6 +8,18 @@ const navi = document.querySelector(".nav-sidebar");
 knop.addEventListener('click', function () {
     navi.style.visibility = "visible"
 });
+
+//buttons
+
+const lichaam = document.querySelector("body");
+
+const knopred = document.querySelector("#red");
+
+knopred.addEventListener('click', function () {
+    lichaam.style.backgroundColor = "red"
+    navi.style.visibility = "hidden"
+});
+
 
 
 
